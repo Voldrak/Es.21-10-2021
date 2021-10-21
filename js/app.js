f736c598642861d6f658a8bd55b348319f52ad81
Marco Messina <voldrak94@gmail.com>
2021-10-21 18:01:48 +0200

    Esercizio quasi finito

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 const cart = [];
+
+function myCart(){
+    let prom = prompt("inserisci cosa vuoi comprare");
+
+    let scelta = prom.split(" ");
+
+    cart.push(scelta);
+
+    console.log(cart);
+
+    return cart;
+ }
